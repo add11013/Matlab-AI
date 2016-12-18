f8a56e17f5aaff7ee2a7f09bbfaedcf36866d41c
Roderick <j8888888871@yahoo.com.tw>
2016-12-18 23:51:01 +0800

    1218

--- 1212_PSO/PSO.m
+++ 1212_PSO/PSO.m
@@ -2,65 +2,84 @@ clear
 clc
 close all;
 %% target
-point=50;
-x1=linspace(-1,1,point);
-x2=linspace(-1,1,point);
-k=1;
-for i=1:point
-    for j=1:point
-        target(i,j)=x1(i).^2+x2(j).^2+10;        
-        y(k)=x1(i).^2+x2(j).^2+10;          
-        PP{k}=[x1(i),x2(j)];
-        k=k+1;
-    end
+x=linspace(-2,2);
+for i=1:100
+    y(i)=x(i).^2+10;
+end
+plot(x,y);
+ca={[5,10] [10,10] [-1,10] [1,10]};
+for i=1:4
+    a0(i)=0;
+    a1(i)=0;
+    a2(i)=0;
 end
 
-mesh(x1,x2,target);
 
 
 %% initialization
 swarm_size = 64;                       % number of the swarm particles
-maxIter = 100;                          % maximum number of iterations
+maxIter = 100;                         % maximum number of iterations
 inertia = 1;                           % W
 correction_factor = 2.0;               % c1,c2
-velocity(1:swarm_size,1:2) = 0;                      % set initial velocity for particles
-pbest(1:swarm_size) = 1000;                   % pbest
+velocity(1:swarm_size,1:20) = 0;       % set initial velocity for particles
+pbest(1:swarm_size) = 1000;            % pbest distance
 gbest=1;
-
+gbestDistance=1000;
+s=0;
 for i=1:swarm_size
-            swarm(i,1)=-1+2*rand(1);
-            swarm(i,2)=-1+2*rand(1);
-            pbestPos(i,1)=1000;
-            pbestPos(i,2)=1000;
+    swarm(i,1)=ca{1}(1);
+    swarm(i,2)=ca{1}(2);
+    swarm(i,3)=a0(1);
+    swarm(i,4)=a1(1);
+    swarm(i,5)=a2(1);
+    swarm(i,6)=ca{1}(1);
+    swarm(i,7)=ca{1}(1);
+    swarm(i,8)=a0(2);
+    swarm(i,9)=a1(2);
+    swarm(i,10)=a2(2);
+    swarm(i,11)=ca{1}(1);
+    swarm(i,12)=ca{1}(1);
+    swarm(i,13)=a0(3);
+    swarm(i,14)=a1(3);
+    swarm(i,15)=a2(3);
+    swarm(i,16)=ca{1}(1);
+    swarm(i,17)=ca{1}(1);
+    swarm(i,18)=a0(4);
+    swarm(i,19)=a1(4);
+    swarm(i,20)=a2(4);
 end
     
 
 %% main loop
-for i=1:maxIter
-
-    
-    %move
-    swarm(:,1) = swarm(:,1) + velocity(:,1);
-    swarm(:,2) = swarm(:,2) + velocity(:,2);
-    
-    %caculate error
-    for i=1:swarm_size
-        for j=1:2375
-            yHead(j)=(PP{j}(1)-swarm(i,1)).^2+(PP{j}(2)-swarm(i,2)).^2+10;
-        end
-            error=sum((y(1:2375)-yHead(1:2375))).^2/2375;
-        if error<pbest(i)
-            pbestPos(i,:)=swarm(i,:);
-            pbest(i)=error;
-            if error<pbest(gbest)
-                gbest=i;
+for ite=1:maxIter
+    for k=1:swarm_size
+        %move
+        swarm(k,:)=velocity(k,:)+swarm(k,:);
+        yHead=0;
+        for i=1:98
+            for j=1:4
+                B(j)=gaussmf(y(i),ca{j})*gaussmf(y(i+1),ca{j});
+                R(j)=B(j)*a0(j)+a1(j)*y(i)+a2(j)*y(i+1);
             end
+            for j=1:4
+                yHead=B(j)*R(j)+yHead;
+            end
+            yHead=yHead/sum(B);
+            error(i)=y(i+2)-yHead;
         end
-        for j=2376:2500
-            yHead(j)=(PP{j}(1)-swarm(i,1)).^2+(PP{j}(2)-swarm(i,2)).^2+10;
+        for i=1:98
+            s=error(i)+s;
         end
-            errorTest=sum((y(2376:2500)-yHead(2376:2500))).^2/125;
-    end
+            e=s/98;  
+    
+        if e<pbest(k)
+            pbestPos(k,:)=swarm(k,:);
+            pbest(k)=e;
+        end
+        if e<gbestDistance
+            gbest=k;
+        end
+     end
     
     
     %change velocity
@@ -74,13 +93,6 @@ end
 
 %% result
 
-figure(2)
-for i=1:point
-    for j=1:point
-        result(i,j)=(x1(i)-swarm(gbest,1)).^2+(x2(j)-swarm(gbest,2)).^2+10;        %mesh z
-    end
-end
-mesh(x1,x2,result);
-axis([-1 1 -1 1 10 12])
+
 
 
--- 1212_PSO/main.m
+++ 1212_PSO/main.m
@@ -1,82 +1,64 @@
 clear
 clc
 close all;
-%% target
-point=50;
-x1=linspace(-1,1,point);
-x2=linspace(-1,1,point);
-k=1;
-for i=1:point
-    for j=1:point
-        target(i,j)=x1(i).^2+x2(j).^2+10;        
-        y(k)=x1(i).^2+x2(j).^2+10;          
-        PP{k}=[x1(i),x2(j)];
-        k=k+1;
-    end
+
+x=linspace(-2,2);
+for i=1:100
+    y(i)=x(i)^2+10;
+end
+plot(x,y);
+
+c11=4;a11=10;c21=10;a21=10;c12=4;a12=10;c22=10;a22=10;
+for i=2:100
+    x1=y(i);
+    x2=y(i-1);
+    
 end
 
-mesh(x1,x2,target);
 
 
 %% initialization
-swarm_size = 100;                       % number of the swarm particles
-maxIter = 100;                          % maximum number of iterations
+swarm_size = 64;                       % number of the swarm particles
+maxIter = 500;                         % maximum number of iterations
 inertia = 1;                           % W
 correction_factor = 2.0;               % c1,c2
-velocity(:,1) = 0;                      % set initial velocity for particles
-velocity(:,2)=0;
-pbest(1:swarm_size) = 1000;                   % pbest
+for i=1:swarm_size
+    velocity(i,1:14) = 0;              % set initial velocity for particles
+    swarm(i,1)=c11;                      %c11
+    swarm(i,2)=a11;                     %a11
+    swarm(i,3)=c21;                     %c21
+    swarm(i,4)=a21;                     %a21
+    swarm(i,5:7)=0;                    %A01?A11?A21
+    swarm(i,8)=c12;                      %c12
+    swarm(i,9)=a12;                     %a12
+    swarm(i,10)=c22;                    %c22
+    swarm(i,11)=a22;                    %a22
+    swarm(i,12:14)=0;                  %A02?A12?A22
+end
+pbest(1:swarm_size) = 1000;            % pbest
 gbest=1;
 
-for i=1:swarm_size
-            swarm(i,:)=-1+2*rand(1);
-            swarm(i,2)=-1+2*rand(1);
-end
     
 
 %% main loop
-for i=1:maxIter
-
-    
-    %move
-    swarm(:,1) = swarm(:,1) + velocity(:,1);
-    swarm(:,2) = swarm(:,2) + velocity(:,2);
-    
-    %caculate error
-    for i=1:swarm_size
-        for j=1:2375
-            yHead(j)=(PP{j}(1)-swarm(i,1)).^2+(PP{j}(2)-swarm(i,2)).^2+10;
-        end
-            error=sum((y(1:2375)-yHead)).^2/2375;
-        if error<pbest(i)
-            pbestP(i,:)=swarm(i,:);
-            pbest(i)=error;
-            if error<pbest(gbest)
-                gbest=i;
-            end
-        end
-        
-    end
-    
-    
-    %change velocity
-    for i=1:swarm_size
-        A=inertia*velocity(i,:);%??
-        B=correction_factor*rand(1)*0.1*(pbestP(i,:)-swarm(i,:));%pbest
-        C=correction_factor*rand(1)*0.1*(pbestP(gbest,:) - swarm(i,:));%gbest
-        velocity(i,:)=A+B+C;
-    end
-end
+% for i=1:maxIter
+% 
+%     
+%     %move
+%     swarm(:,:) = swarm(:,:) + velocity(:,:);
+%     
+%     
+%     
+%     %change velocity
+%     for i=1:swarm_size
+%         A=inertia*velocity(i,:);%??
+%         B=correction_factor*rand(1)*0.1*(pbestPos(i,:)-swarm(i,:));%pbest
+%         C=correction_factor*rand(1)*0.1*(pbestPos(gbest,:) - swarm(i,:));%gbest
+%         velocity(i,:)=A+B+C;
+%     end
+% end
 
 %% result
 
-
-figure(2)
-for i=1:point
-    for j=1:point
-        result(i,j)=(x1(i)-swarm(gbest,1)).^2+(x2(j)-swarm(gbest,2)).^2+10;        %mesh z
-    end
-end
-mesh(x1,x2,result);
 
 
