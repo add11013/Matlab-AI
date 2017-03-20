6ccfdd6aa8dfb76f28421dd8065b37029dae124e
Roderick <j8888888871@yahoo.com.tw>
2017-03-21 00:01:35 +0800

    fitdist

--- 0312_fitdist/main.m
+++ 0312_fitdist/main.m
@@ -1,18 +1,28 @@
+clear,clc
 figure(1);
 %t=fetch(yahoo,'^GSPC','Close','1/1/2016','12/31/2016','d');
 t=linspace(1,252,252);
 point=length(t);
-x=linspace(1,point,point);
 %y=flipud(t(:,2));
-for i=1:point 
+for i=1:point
+    %y(i,1)=t(i,2);
     y(i,1)=t(1,i);
 end
 yMean=mean(y);
 yStd=std(y);
 
+x=linspace(yMean-5*yStd,yMean+5*yStd,point);
 plot(x,y);
 
 pd=fitdist(y,'normal');
-sample=pdf(pd,y);
+
 figure(2)
+sample=pdf(pd,x);
 plot(x,sample);
+%range
+for i=1:point
+    p(i)=-sample(i)*log2(sample(i));
+end
+P=sum(p)
+
+
